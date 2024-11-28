dc382f3a851c1969ac2e5f88e9964bd4e63fd6a8
Michal Stawicki <michal@procode.io>
2024-11-28 10:24:19 +0100

    cdk virtual scroll bug repro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CdkFixedSizeVirtualScroll, CdkVirtualForOf, CdkVirtualScrollViewport } from '@angular/cdk/scrolling'
+import { MatButton } from '@angular/material/button'
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CdkVirtualScrollViewport,
+    CdkFixedSizeVirtualScroll,
+    CdkVirtualForOf,
+    MatButton,
+    JsonPipe,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'virtual-scroll-repro';
+
+  items = [0]
+
+  constructor(public cd: ChangeDetectorRef) {
+
+  }
+
+  updateItems(count: number) {
+    this.items = []
+    for (let i = 0; i < count + 1; i++) {
+      this.items.push(i)
+    }
+
+    this.cd.markForCheck()
+  }
 }
